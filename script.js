e34a398ca139b2a7ab2e6ed12ee334cb5415678e
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-04 16:26:48 +0100

    arrays

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 setTimeout(function(){
     document.getElementById('numbers').classList.add("dnone");
-}, 2500);
+}, 25000);
+const randomNums = [];
+const userNumsArray = [];
+const indovinati = [];
 
 setTimeout(function(){
     for (let index = 0; index < 5; index++) {
-        prompt('Inserisci un numero:');  
+        const userNum=parseInt(prompt('Inserisci un numero:'));
+        userNumsArray.push(userNum);
+        if (randomNums.includes(userNum)){
+            indovinati.push(userNum);
+        }
     }
-}, 3000);
+
+    console.log(userNumsArray);
+    console.log(indovinati);
+}, 30000);
 
 randomGenerator(100);
-
+console.log(randomNums);
 function randomGenerator (max){
     for (let index = 1; index <= 5; index++) {
         num=Math.floor(Math.random()*max+1);
+        randomNums.push(num);
         const element=document.createElement("div");
         element.innerHTML=num;
         document.getElementById("numbers").append(element);
     }
 }
-
